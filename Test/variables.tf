4463dc4c41d3e72e12f93d3745760ad21f87412f
Venkataramana Lakka -X (vlakka - COGNIZANT TECHNOLOGY SOLUTIONS INDIA PVT LTD at Cisco) <vlakka@cisco.com>
2017-03-07 17:28:48 +0530

    First commit

---
+++ Test/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+	default = "AKIAIQUIXA4Z2VZGRH2Q"
+}
+
+variable "aws_secret_key" {
+	default = "HlJv5QYQyM1yV287CfNXNocwIA8iCDpKTXMAX+cj"
+}
+
+variable "key_name" {
+	default = "vlakka_key"
+	description = "Key name to use"
+}
+variable "dcname" {
+	description = "The data-center name."
+}
